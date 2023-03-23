1fdbfb5e407fdfa88046ea5fe199138b18abf603
sbmalik <m3sibti@gmail.com>
2023-03-23 12:28:11 +0500

    formatting

--- t001_stable_diffusion_scratch/dataloader.py
+++ t001_stable_diffusion_scratch/dataloader.py
@@ -6,13 +6,17 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 import cv2
 from PIL import Image
+from config import *
+import matplotlib.pyplot as plt
 import base64
+
 
 def to_device(data, device):
     """Move tensor(s) to chosen device"""
     if isinstance(data, (list, tuple)):
         return [to_device(x, device) for x in data]
     return data.to(device, non_blocking=True)
+
 
 class DeviceDataLoader:
     """Wrap a dataloader to move data to a device"""
@@ -30,15 +34,18 @@ class DeviceDataLoader:
         """Number of batches"""
         return len(self.dl)
 
+
 def get_default_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def save_images(images, path, **kwargs):
     grid = make_grid(images, **kwargs)
     ndarr = grid.permute(1, 2, 0).to("cpu").numpy()
     im = Image.fromarray(ndarr)
     im.save(path)
-    
+
+
 def get(element: torch.Tensor, t: torch.Tensor):
     """
     Get value at index position "t" in "element" and
@@ -47,13 +54,17 @@ def get(element: torch.Tensor, t: torch.Tensor):
     ele = element.gather(-1, t)
     return ele.reshape(-1, 1, 1, 1)
 
+
 def setup_log_directory(config):
-    '''Log and Model checkpoint directory Setup'''
-    
+    """Log and Model checkpoint directory Setup"""
+
     if os.path.isdir(config.root_log_dir):
         # Get all folders numbers in the root_log_dir
-        folder_numbers = [int(folder.replace("version_", "")) for folder in os.listdir(config.root_log_dir)]
-        
+        folder_numbers = [
+            int(folder.replace("version_", ""))
+            for folder in os.listdir(config.root_log_dir)
+        ]
+
         # Find the latest version number present in the log_dir
         last_version_number = max(folder_numbers)
 
@@ -63,27 +74,28 @@ def setup_log_directory(config):
     else:
         version_name = config.log_dir
 
-    # Update the training config default directory 
-    log_dir        = os.path.join(config.root_log_dir,        version_name)
+    # Update the training config default directory
+    log_dir = os.path.join(config.root_log_dir, version_name)
     checkpoint_dir = os.path.join(config.root_checkpoint_dir, version_name)
 
     # Create new directory for saving new experiment version
-    os.makedirs(log_dir,        exist_ok=True)
+    os.makedirs(log_dir, exist_ok=True)
     os.makedirs(checkpoint_dir, exist_ok=True)
 
     print(f"Logging at: {log_dir}")
     print(f"Model Checkpoint at: {checkpoint_dir}")
-    
+
     return log_dir, checkpoint_dir
+
 
 def frames2vid(images, save_path):
 
     WIDTH = images[0].shape[1]
     HEIGHT = images[0].shape[0]
 
-#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#     fourcc = 0
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    #     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    #     fourcc = 0
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     video = cv2.VideoWriter(save_path, fourcc, 25, (WIDTH, HEIGHT))
 
     # Appending the images to the video one by one
@@ -93,7 +105,8 @@ def frames2vid(images, save_path):
     # Deallocating memories taken for window creation
     # cv2.destroyAllWindows()
     video.release()
-    return 
+    return
+
 
 # def display_gif(gif_path):
 #     b64 = base64.b64encode(open(gif_path,'rb').read()).decode('ascii')
@@ -104,35 +117,63 @@ def get_dataset():
     transforms = TF.Compose(
         [
             TF.ToTensor(),
-            TF.Resize((32, 32), 
-                      interpolation=TF.InterpolationMode.BICUBIC, 
-                      antialias=True),
+            TF.Resize(
+                (32, 32), interpolation=TF.InterpolationMode.BICUBIC, antialias=True
+            ),
             TF.RandomHorizontalFlip(),
-            TF.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1] 
+            TF.Lambda(lambda t: (t * 2) - 1),  # Scale between [-1, 1]
         ]
     )
-    
-    dataset = datasets.ImageFolder(root="/home/sbmalik/workspace/research/gen_ai/datasets/flowers", 
-                                   transform=transforms)
-    
+
+    dataset = datasets.ImageFolder(
+        root="/home/sbmalik/workspace/research/gen_ai/datasets/flowers",
+        transform=transforms,
+    )
+
     return dataset
 
-def get_dataloader(dataset_name='MNIST', 
-                   batch_size=32, 
-                   pin_memory=False, 
-                   shuffle=True, 
-                   num_workers=0, 
-                   device="cpu"
-                  ):
-    dataset    = get_dataset(dataset_name=dataset_name)
-    dataloader = DataLoader(dataset, batch_size=batch_size, 
-                            pin_memory=pin_memory, 
-                            num_workers=num_workers, 
-                            shuffle=shuffle
-                           )
+
+def get_dataloader(
+    batch_size=32,
+    pin_memory=False,
+    shuffle=True,
+    num_workers=0,
+    device="cpu",
+):
+    dataset = get_dataset()
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        pin_memory=pin_memory,
+        num_workers=num_workers,
+        shuffle=shuffle,
+    )
     device_dataloader = DeviceDataLoader(dataloader, device)
     return device_dataloader
 
+
 def inverse_transform(tensors):
     """Convert tensors from [-1., 1.] to [0., 255.]"""
-    return ((tensors.clamp(-1, 1) + 1.0) / 2.0) * 255.0 
+    return ((tensors.clamp(-1, 1) + 1.0) / 2.0) * 255.0
+
+
+if __name__ == "__main__":
+    loader = get_dataloader(
+        batch_size=8,
+        device="cpu",
+    )
+    plt.figure(figsize=(12, 6), facecolor="white")
+
+    for b_image, _ in loader:
+        b_image = inverse_transform(b_image).cpu()
+        grid_img = make_grid(
+            b_image / 255.0,
+            nrow=16,
+            padding=True,
+            pad_value=1,
+            normalize=True,
+        )
+        plt.imshow(grid_img.permute(1, 2, 0))
+        plt.axis("off")
+        plt.show()
+        break
